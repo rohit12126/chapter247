db5b1dbcf8621d078e2fc033959f276c42aadd78
rohita.chapter247@gmail.com <you@example.com>
2018-12-27 16:05:52 +0530

    1 days

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import Person from './Person/Person';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      persons: [
+        {id: '34',name: 'SH', age:28},
+        {id: 'hfg',name: 'dsf', age: 34}
+      ]
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick(arg, event) {
+    const persons = [...this.state.persons];
+    const a = persons.push({name:'jj'});
+   
+    console.log(a);
+    this.setState({
+      persons: [{ name:'fg', age:45 }]
+    })
+
+    console.log(this.state);
+  }
+
+  render() {
+    console.log(this.state.persons);
+    const persons = this.state.persons.map((person) => {
+      return (<Person key={person.id} name={person.name} >Age: {person.age}</Person>)
+    })
+    return (
+      <div className="App">
+        <h1>Hello World</h1>
+        <button onClick={this.handleClick}>Click</button>
+        {persons}
+      </div>
+    );
+  }
+}
+
+export default App;
