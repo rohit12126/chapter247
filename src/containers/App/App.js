06e1c7f49b5f362dd51089ff22402d16d68bb20b
Tushar Goyal <tushar.chapter247@gmail.com>
2018-12-29 16:33:19 +0530

    Make structure ready and moving forward to task

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-import Char from './Components/character/char';
-import Validation from './Components/validation/validation';
 
 class App extends Component {
   constructor() {
@@ -9,20 +7,6 @@ class App extends Component {
     this.state = {
       name: '',
     }
-    this.handleInputs = this.handleInputs.bind(this);
-    this.manipulateInput = this.manipulateInput.bind(this);
-  }
-
-
-  handleInputs(e) {
-    this.setState({[e.target.name]: e.target.value});
-  }
-
-  manipulateInput(index) {
-    let n = this.state.name;
-    n = n.split('');
-    n.splice(index, 1);
-    this.setState({ name: n.join('')});
   }
 
 
@@ -30,15 +14,7 @@ class App extends Component {
     
     return (
       <div className="App">
-        <input type="text" placeholder="Name" name="name" value={this.state.name} onChange={this.handleInputs} />
-
-        <br />
-
-        <Validation name={this.state.name} type={'Name'} />
-
-        <br />
-
-        <Char name={this.state.name} removeFromInput={this.manipulateInput} />
+        This is root component!
       </div>
     );
   }
